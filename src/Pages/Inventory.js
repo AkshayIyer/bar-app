b3e3c485af971c43ade45add725560704423f371
Iyer, Akshay R <aiyer89@gatech.edu>
2020-11-14 12:48:10 -0600

    Bar Employee Insights

--- src/Pages/Inventory.js
+++ src/Pages/Inventory.js
@@ -1,15 +1,20 @@
 
 import ReactDOM from 'react-dom';
 import React, {useState, useEffect} from "react";
-
 import Table from 'react-bootstrap/Table'
-
+import { Button } from 'reactstrap';
+import { Link } from 'react-router-dom'
 export default function Inventory(props) {
     return(
         <div className="Inventory">
-        <h1> Table </h1>
-
-        
+        <Button className = "HelpButton"> ? </Button>
+        <Link to = "/admin">
+            <Button className = "BackAdminButton">Back to Statistics</Button>
+        </Link>
+        <h1 class = "Table_Text"> Bartender Insights </h1>
+        <p class = "Pouring_Targets"> Pouring targets: 75%</p>
+        <p class = "Overpouring_Average"> Overpouring average: 50%</p>
+        <Button className = "filterButton">Filters: 3/16/2018; Shift(9AM-12PM);...</Button>
         <Table className = "Table-header">
         <colgroup>
           <col class = "green"/>
@@ -30,14 +35,58 @@ export default function Inventory(props) {
             <td>6</td>
             <td>3</td>
             </tr>
+            
+            
             <tr>
-      
             <td>Jane Doe</td>
             <td>7</td>
             <td>6</td>
             <td>1</td>
             </tr>
+
+            <tr>
+            <td>Toph Beifong</td>
+            <td>12</td>
+            <td>7</td>
+            <td>5</td>
+            </tr>
+
+            <tr>
+            <td>Katara</td>
+            <td>10</td>
+            <td>10</td>
+            <td>0</td>
+            </tr>
             
+            <tr>
+            <td>Zuko</td>
+            <td>11</td>
+            <td>9</td>
+            <td>2</td>
+            </tr>
+
+            <tr>
+            <td>Aang</td>
+            <td>6</td>
+            <td>4</td>
+            <td>2</td>
+            </tr>
+
+            <tr>
+            <td>Sokka</td>
+            <td>13</td>
+            <td>10</td>
+            <td>3</td>
+            </tr>
+
+            <tr>
+            <td>Suki</td>
+            <td>8</td>
+            <td>7</td>
+            <td>1</td>
+            </tr>
+
+
         </tbody>
         </Table>
         
